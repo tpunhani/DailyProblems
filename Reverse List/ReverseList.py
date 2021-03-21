f059baf29041157eb2ae4f3483f9cd6b24cfd57b
Tarun Punhani <tpunhani@gmail.com>
2021-03-21 11:48:11 -0500

    Reverse List Python

---
+++ Reverse List/ReverseList.py
@@ -0,0 +1,11 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+def reverseList(list, start, end) -> list:
+    if start >= end:
+        return list
+
+    list[start], list[end] = list[end], list[start]
+    return rev(list, start+1, end-1)
+
+
