146a732ffd5a8f2a217736ca2d572e3b374215b0
Tarun Punhani <tpunhani@gmail.com>
2021-03-21 12:08:33 -0500

    Added iterative solution

--- Reverse List/ReverseList.py
+++ Reverse List/ReverseList.py
@@ -1,11 +1,23 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-def reverseList(list, start, end) -> list:
+
+# reverse the list recursively
+def reverseListRecursive(list, start, end) -> list:
     if start >= end:
         return list
 
     list[start], list[end] = list[end], list[start]
-    return reverseList(list, start+1, end-1)
+    return reverseListRecursive(list, start+1, end-1)
+
+
+# In place iterative solution with two pointers
+def reverseListIterative(list) -> list:
+    i=0
+    j=len(list)-1
+
+    while i < j:
+        list[i], list[j] = list[j], list[i]
+        i += 1
+        j -= 1
+    return list
+
 
 
